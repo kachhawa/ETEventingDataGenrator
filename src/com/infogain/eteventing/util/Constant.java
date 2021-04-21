95fbdfdc8a5de7df0315a91517da0ffdeee8c6a5
Ajay Kachhawa <ajay.kachhawa@gmail.com>
2021-04-21 20:47:08 +0530

    poc

---
+++ src/com/infogain/eteventing/util/Constant.java
@@ -0,0 +1,22 @@
+package com.infogain.eteventing.util;
+
+public class Constant {
+	public final static String COMMA = ","; 
+	public final static String BLANK_VALUE = ""; 
+	public final static String UNDERSCORE = "_";
+	public final static String CSV_EXT = ".csv";
+	public final static String CSV_HEADER = "locator,version,parent_locator,created,data";
+	public final static String DTO_DATA	= "\\\"{\\\"pnr\\\":\\\"{}\\\"\\\"cart\\\":\\\"{}\\\"\\\"session\\\":\\\"{}\\\"";
+	public final static String NEWLINE = "\n";
+	public final static Integer ONE = 1;
+	public final static Integer TWO = 2;
+	public final static Integer TEN = 10;
+	public final static Integer TWENTY = 20;
+	public final static Integer THIRTY = 30;
+	public final static Integer TEN_THOUSAND = 10000;
+	public final static Integer ONE_LAKH = 10000;
+	
+	public final static Integer ASCII_A = 65;
+	public final static Integer ASCII_Z = 90;
+		
+}
