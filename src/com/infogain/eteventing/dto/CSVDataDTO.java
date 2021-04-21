14581e2687cfd65cc7eaa0542bdde9cf7825439e
Ajay Kachhawa <ajay.kachhawa@gmail.com>
2021-04-21 23:46:33 +0530

    lombok configuration

--- src/com/infogain/eteventing/dto/CSVDataDTO.java
+++ src/com/infogain/eteventing/dto/CSVDataDTO.java
@@ -1,34 +1,15 @@
 package com.infogain.eteventing.dto;
 
-public class CSVDataDTO {
-	String locator;
-	String version;
-	String parent_locator;
-	String data;
-	public String getLocator() {
-		return locator;
-	}
-	public void setLocator(String locator) {
-		this.locator = locator;
-	}
-	public String getVersion() {
-		return version;
-	}
-	public void setVersion(String version) {
-		this.version = version;
-	}
-	public String getParent_locator() {
-		return parent_locator;
-	}
-	public void setParent_locator(String parent_locator) {
-		this.parent_locator = parent_locator;
-	}
-	public String getData() {
-		return data;
-	}
-	public void setData(String data) {
-		this.data = data;
-	}
+import lombok.Getter;
+import lombok.Setter;
 
-	
+public class CSVDataDTO {
+	@Getter @Setter
+	String locator;
+	@Getter @Setter
+	String version;
+	@Getter @Setter
+	String parent_locator;
+	@Getter @Setter
+	String data;
 }
